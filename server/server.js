a1aecb401288bfde2581b78284cb44f0f83e2132
mPemba <michael.j.say@gmail.com>
2016-01-30 23:41:28 -0700

    email not working

--- server/server.js
+++ server/server.js
@@ -6,12 +6,12 @@ Meteor.publish( "carts", function() {
   return Carts.find({});
 });
 
-Meteor.startup( function() {
-  process.env.MAIL_URL = "sandboxfb3d66b5c7764dc9889573755876171f.mailgun.org";
-});
+// Meteor.startup( function() {
+//   process.env.MAIL_URL = "sandboxfb3d66b5c7764dc9889573755876171f.mailgun.org";
+// });
 
 Meteor.startup( function() {
-  process.env.MAIL_URL = "smtp://postmaster%40104.130.122.3.mailgun.org:password@smtp.mailgun.org:587";
+  process.env.MAIL_URL = "smtp://postmaster@sandboxfb3d66b5c7764dc9889573755876171f.mailgun.org:7c07e3eb9ec6c2b69fb6de109d28712d@smtp.mailgun.org:587";
 });
 
 Meteor.methods({
@@ -20,5 +20,19 @@ Meteor.methods({
 
     currentCart.items.push(shirt);
 
+  },
+  sendEmail: function (to, from, subject, text) {
+    check([to, from, subject, text], [String]);
+
+    // Let other method calls from the same client start running,
+    // without waiting for the email sending to complete.
+    this.unblock();
+
+    Email.send({
+      to: to,
+      from: from,
+      subject: subject,
+      text: text
+    });
   }
 });
