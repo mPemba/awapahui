b2a7da006dae78d6923b5ba59e80546966c6f816
mPemba <michael.j.say@gmail.com>
2016-02-03 17:18:10 -0700

    favicon and some other styles

--- server/server.js
+++ server/server.js
@@ -6,12 +6,24 @@ Meteor.publish( "carts", function() {
   return Carts.find({});
 });
 
-// Meteor.startup( function() {
-//   process.env.MAIL_URL = "sandboxfb3d66b5c7764dc9889573755876171f.mailgun.org";
-// });
-
 Meteor.startup( function() {
+
+
   process.env.MAIL_URL = "smtp://postmaster@sandboxfb3d66b5c7764dc9889573755876171f.mailgun.org:7c07e3eb9ec6c2b69fb6de109d28712d@smtp.mailgun.org:587";
+
+
+
+
+  // smtp = {
+  //   username: Meteor.settings.private.mailgun.username,
+  //   password: Meteor.settings.private.mailgun.password,
+  //   server: Meteor.settings.private.mailgun.host,
+  //   port: Meteor.settings.private.mailhun.port
+  // }
+  //
+  // process.env.MAIL_URL = process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+
+
 });
 
 Meteor.methods({
